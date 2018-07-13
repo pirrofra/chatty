a1344db45851611a355dea2e1d287ae352786d7f
pirrofra <37002187+pirrofra@users.noreply.github.com>
2018-07-13 11:48:27 +0200

    Add files via upload

---
+++ chatty.c
@@ -0,0 +1,37 @@
+/*
+ * membox Progetto del corso di LSO 2017/2018
+ *
+ * Dipartimento di Informatica Università di Pisa
+ * Docenti: Prencipe, Torquati
+ * 
+ */
+/**
+ * @file chatty.c
+ * @brief File principale del server chatterbox
+ */
+#define _POSIX_C_SOURCE 200809L
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <assert.h>
+#include <string.h>
+#include <signal.h>
+#include <pthread.h>
+
+/* inserire gli altri include che servono */
+
+/* struttura che memorizza le statistiche del server, struct statistics 
+ * e' definita in stats.h.
+ *
+ */
+struct statistics  chattyStats = { 0,0,0,0,0,0,0 };
+
+
+static void usage(const char *progname) {
+    fprintf(stderr, "Il server va lanciato con il seguente comando:\n");
+    fprintf(stderr, "  %s -f conffile\n", progname);
+}
+
+int main(int argc, char *argv[]) {
+    return 0
+}
